eee15426d34f0fbb3d9fd64ed5db6aa644e3d841
Rutwik Kulkarni <rutwikk.1@gmail.com>
2023-11-12 23:21:19 +0000

    Added basic error handling to request

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,49 @@
 import {useState} from "react";
-import {Button, Card, CardBody, CardHeader, Flex, Heading, Input, Spinner, Text} from "@chakra-ui/react";
+import {
+    Alert, AlertDescription,
+    AlertIcon, AlertTitle,
+    Button,
+    Card,
+    CardBody,
+    CardHeader,
+    Flex,
+    Heading,
+    Input,
+    Spinner,
+    Text
+} from "@chakra-ui/react";
 
 function App() {
     const [loading, setLoading] = useState(false)
     const [url, setUrl] = useState('')
     const [summary, setSummary] = useState('')
     const [summaryTitle, setSummmaryTitle] = useState('')
+    const [error, setError] = useState(false)
 
     const summarise = async () => {
+        setError(false)
         setLoading(true)
-        let response = await fetch('http://localhost:5000/summarise/youtube?' + new URLSearchParams({
-            url: url
-        }))
+        let response
+        try {
+            response = await fetch('http://localhost:5000/summarise/youtube?' + new URLSearchParams({
+                url: url
+            }))
+        } catch (e) {
+            setLoading(false)
+            setError(true)
+            console.error(e)
+            return
+        }
+        console.log(response)
+        setLoading(false)
+        if (response.status !== 200) {
+            setError(true)
+            return
+        }
+
         let body = await response.json()
         setSummmaryTitle(body['title'])
         setSummary(body['summary'])
-        setLoading(false)
     }
 
     return (
@@ -28,13 +56,27 @@ function App() {
                 placeholder='YouTube URL'
                 onChange={e => setUrl(e.target.value)}
             />
+            { error ?
+                <Alert
+                    status='error'
+                    mt={2}
+                    width={{base: '90%', lg: '60%'}}
+                    borderRadius={3}
+                >
+                    <AlertIcon />
+                    <AlertTitle>Error</AlertTitle>
+                    <AlertDescription>Failed to fetch video. Either the server is down or the URL is invalid.</AlertDescription>
+                </Alert> :
+                <div></div>
+            }
+
             { loading ?
                 <Spinner
                     mt={4}
                 /> :
                 <Button
                     mt={4}
-                    onClick={() => summarise()}
+                    onClick={async () => await summarise()}
                     backgroundColor='green.500'
                     color='white'
                 >
